09b8956d7160ab9d6f6c4fd9bbc26223510db501
Jayden Grubb <jaydengrubb1@gmail.com>
2025-06-26 19:59:18 +1000

    Bump CMake and C++ versions

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
-#include <cstdio>
+#include <print>
 #include <cstdlib>
 
 int main(int argc, char **argv) {
-	std::printf("Hello, World!\n");
+	std::println("Hello, World!");
 
 	for (int i = 0; i < argc; i++) {
-		std::printf("argv[%d] = %s\n", i, argv[i]);
+		std::println("argv[{}] = {}", i, argv[i]);
 	}
 
 	return EXIT_SUCCESS;
